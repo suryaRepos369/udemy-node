a93c4783824dd1dff51060ec4113d5a2f96079ac
surya <suryaprakasam@innoboon.com>
2024-02-26 19:14:30 +0530

    mvc boilerplate added

--- db/db.js
+++ db/db.js
@@ -3,9 +3,11 @@ const mongoose = require ('mongoose')
  const dbConnect = async ()=>{
    console.log('connecting mongo db ')
     try {
-        const a = await mongoose.connect('mongodb://127.0.0.1:27017/blog_app')
-        console.log('successfully connected to db')
         
+        const username ='admin';
+        const password ='password'
+        const a = await mongoose.connect(`mongodb://${username}:${password}@localhost:27017`)
+        console.log('successfully connected to db')
     } catch (error) {
         
         console.log('Error connecting db ......',error)
--- router/blog/router.js
+++ router/blog/router.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Blog = require('./../../models/blog.model')
+router.get('/list', async(req,res)=>{
+    try {
+        const data = await Blog.find({});
+        if(data&&data.length>0){
+            res.send(data)
+        }
+        else{
+            res.send({
+                response:'No data found'
+            })
+        }
 
-router.get('/list', (req,res)=>{
-    res.send('list of users')
+    } catch (error) {
+        console.log(error)
+        res.send('Error')
+    }
+   
 })
+
+router.post('/addList', async (req, res) => {
+  try {
+    const reqBody = req.body
+    const newData = new Blog(reqBody)
+    const a = await newData.save()
+    res.status(201).send(a)
+  } catch (error) {
+    res.status(404).send(error)
+  }
+})
+
 
 
 module.exports = router;
