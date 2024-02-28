e048d69124a91025fd09bedb4c5c44d6cd3a641d
surya <suryaprakasam@innoboon.com>
2024-02-28 10:43:24 +0530

    removed log

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -2,7 +2,6 @@ const httpStatus = require('http-status')
 const ApiResponse = require('../utils/ApiResponse')
 const catchAsync = fn => async (req, res, next) => {
   // Promise.resolve(fn(req, res, next)).catch((err)=>next(err))
-  console.log('*********************formating*result ', fn)
   try {
     const result = await fn(req, res, next)
     if (result instanceof ApiResponse) {
