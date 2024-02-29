08c4c489050b529879336b477a51c0ea8a80932a
surya <suryaprakasam@innoboon.com>
2024-02-29 16:02:55 +0530

    husky config

--- models/user.js
+++ models/user.js
@@ -1,30 +1,42 @@
-const mongoose = require('mongoose')
-const userSchema = mongoose.Schema({
-
-    name:{
-        type:String,
-        required:true,
-        trim:true
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail()) throw new Error("Invalid email");
+      },
     },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:5
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      validate(value) {
+        if (!validator.isStrongPassword(value))
+          throw new Error(
+            "Password should contain atleast a uppercase, a lowercase , a number and a special character",
+          );
+      },
     },
-},{timestamps:true});
+  },
+  { timestamps: true },
+);
 
-userSchema.statics.isEmailTaken=async function(email){
-    const user = await this.findOne({email})
-    return !!user;
-}
+userSchema.statics.isEmailTaken = async function (email) {
+  const user = await this.findOne({ email });
+  return !!user;
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports  =User;
+module.exports = User;
